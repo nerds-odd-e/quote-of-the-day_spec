f7d93f4ab0c0e4f6251893eefae1244bd5579681
juacompe <juacompe@gmail.com>
2019-01-21 17:30:01 +0800

    Try interacting with a browser

--- features/support/quote-of-the-day-steps.js
+++ features/support/quote-of-the-day-steps.js
@@ -1,13 +1,13 @@
 const { Given, When, Then } = require('cucumber')
+const puppeteer = require('puppeteer');
 
-Given('there is a quote', function (dataTable) {
-// Write code here that turns the phrase above into concrete actions
-return 'pending';
-});
+Given('there is a quote', function (dataTable) { });
 
-When('I visit the quote of the day', function () {
-// Write code here that turns the phrase above into concrete actions
-return 'pending';
+When('I visit the quote of the day', async function () {
+  const browser = await puppeteer.launch({executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'});
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  await browser.close();
 });
 
 Then('I should see the quote', function () {
