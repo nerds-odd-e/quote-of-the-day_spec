04aa2ed2103f5fe2461eddf77acea1272a985211
Bas Vodde <basv@odd-e.com>
2019-01-23 11:29:16 +0800

    Implemented the pending steps

--- features/support/quote-of-the-day-steps.js
+++ features/support/quote-of-the-day-steps.js
@@ -1,16 +1,43 @@
 const { Given, When, Then } = require('cucumber')
 const puppeteer = require('puppeteer');
 
-Given('there is a quote', function (dataTable) { });
+Given('There are the following quotes', function (dataTable) {
+  return 'pending';
+});
 
-When('I visit the quote of the day', async function () {
+Given('The following random quotes map to the days', function (dataTable) {
+  return 'pending';
+});
+
+When('I visit QOFT', async function () {
   const browser = await puppeteer.launch({executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'});
   const page = await browser.newPage();
   await page.goto('http://localhost:8008');
   await browser.close();
 });
 
-Then('I should see the quote', function () {
-// Write code here that turns the phrase above into concrete actions
-return 'pending';
+Then('I should be welcomed with {string}', function (string) {
+  return 'pending';
 });
+
+When('I visit QOFT on {string}', function (string) {
+  return 'pending';
+});
+
+Then('I should see {string}', function (string) {
+  return 'pending';
+});
+
+Then('When I refresh', function () {
+  return 'pending';
+});
+
+Then('When I wait One Day and refresh', function () {
+  return 'pending';
+});
+
+When('my Browser Language is {string}', function (string) {
+  return 'pending';
+});
+
+
