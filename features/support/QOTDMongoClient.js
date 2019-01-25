a667b5fb37a383cab6b20001fa27a2dfa9590af1
Bas Vodde <basv@odd-e.com>
2019-01-25 10:53:35 +0800

    Forgot file

---
+++ features/support/QOTDMongoClient.js
@@ -0,0 +1,48 @@
+
+class QOTDMongoClient {
+
+    constructor() {
+        this.MongoClient = require('mongodb').MongoClient;
+        this.mongoClient = this.MongoClient("mongodb://localhost:27017/");
+    }
+
+    async connect() {
+        await this.mongoClient.connect();
+    }
+
+    disconnect() {
+        this.mongoClient.close();
+    }
+
+    async dropDatabase() {
+        if (this.mongoDB) {
+            await this.mongoDB.dropDatabase();
+        }
+    }
+
+    createDatabase() {
+        this.mongoDB = this.mongoClient.db('quotes-database');
+    }
+
+    async connectToMongoAndCreateDatabase() {
+        await this.connect();
+        this.createDatabase();
+    }
+    async dropDatabaseAndDisconnect() {
+        await this.dropDatabase();
+        this.disconnect();
+    }
+
+    async insertCollectionIntoDatabase(collection, dataTable) {
+        await this.mongoDB.collection('quotes').insertMany(dataTable.hashes());
+    }
+
+    async insertQuotesIntoDatabase(dataTable) {
+        await this.insertCollectionIntoDatabase('quotes', dataTable);
+    }
+
+    async insertDaysWithQuotesIntoDatabase(dataTable) {
+        await this.insertCollectionIntoDatabase('days-with-quotes', dataTable);
+    }
+}
+exports.QOTDMongoClient = QOTDMongoClient;
